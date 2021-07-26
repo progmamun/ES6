aa740608ba0fa252b6238053482e14e0c22cb4d6
progmamun <mamun333089@gmail.com>
2021-07-26 19:25:50 +0600

    initial commit

---
+++ js_problem_solving/swap.js
@@ -0,0 +1,17 @@
+var a = 5;
+var b = 7;
+console.log("Before swap: a =", a, "b =", b);
+var temp = a;
+a = b;
+b = temp;
+console.log("after swap: a =", a, "b =", b);
+/*
+method 2
+
+var x = 5;
+var y = 7;
+x = x + y;
+y = x - y;
+x = x - y;
+console.log("After Swap: x=", x, "y =", y);
+*/
