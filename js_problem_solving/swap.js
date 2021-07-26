2881e7d72968644365ae838123d007b66631b7a4
progmamun <mamun333089@gmail.com>
2021-07-26 19:43:12 +0600

    initial commit 1

--- js_problem_solving/swap.js
+++ js_problem_solving/swap.js
@@ -14,4 +14,12 @@ x = x + y;
 y = x - y;
 x = x - y;
 console.log("After Swap: x=", x, "y =", y);
+
+anothor methods
+
+var p = 5;
+var q = 7;
+
+[p, q] = [q, p];
+console.log("After swap: p =", p, "q =", q);
 */
