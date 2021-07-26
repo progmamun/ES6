4b980faf0251ac661a6cb7d757f53f8121e679f9
progmamun <mamun333089@gmail.com>
2021-07-26 20:08:08 +0600

    math tricks random number etc.

---
+++ js_problem_solving/random.js
@@ -0,0 +1,16 @@
+var num = 2.12458;
+var result = Math.floor(num);
+var result2 = Math.ceil(num); //basi asba
+var result3 = Math.round(num); // 3 asba 2.49557 hola 2. but 2.5 hola 3 asba
+var dice = Math.random();
+//var dice = Math.random() * 10;
+//var output = Math.round(randomNum);
+//console.log = Math.round(randomNum);
+//console.log(dice); //random value diba
+// console.log(result);
+
+// for(var i = 0; i <10; i++){
+//   var randomNum = Math.random() * 6;
+//   var output = Math.round(randomNum);
+//   console.log(output);
+// }
