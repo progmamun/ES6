a118848db6583acb5cc1a4718c3ccc7ea050a8a9
progmamun <mamun333089@gmail.com>
2021-07-27 10:31:35 +0600

    template string es6

---
+++ es6_test/template.js
@@ -0,0 +1,10 @@
+const firstName = "Al Mamun";
+const lastName = "Khan";
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullName2 = `${firstName} ${20+50+30} is a good boy.`;
+console.log(fullName2);
+
+const multiLine = `Hello, there!
+I'm here help you.
+`;
+// console.log(multiLine);
